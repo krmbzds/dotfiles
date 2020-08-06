959229fb1530facc8c5270db95899396247f2fc0
Kerem Bozdas <krmbzds.github@gmail.com>
2020-08-06 23:14:08 +0300

    Update version managers

--- config/.version-managers.sh
+++ config/.version-managers.sh
@@ -3,22 +3,9 @@
 # Version Manager Configurations
 # Source this file in your shell profile to use it
 
-# Rust version manager
-function activate_rustup {
-  path ~/.cargo/bin
-}
-
 # Ruby version manager
 function activate_rvm {
   source ~/.rvm/scripts/rvm
 }
 
-# ASDF version manager
-function activate_asdf {
-  source ~/.asdf/asdf.sh
-  source ~/.asdf/completions/asdf.bash
-}
-
-# activate_rustup
 # activate_rvm
-# activate_asdf
