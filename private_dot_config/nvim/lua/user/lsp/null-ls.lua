18901c3922c4e627e6a0709cdd383977ec00c70a
Kerem Bozdas <krmbzds.github@gmail.com>
2022-07-26 01:51:27 +0300

    Add vale

--- private_dot_config/nvim/lua/user/lsp/null-ls.lua
+++ private_dot_config/nvim/lua/user/lsp/null-ls.lua
@@ -6,7 +6,7 @@ end
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
--- local diagnostics = null_ls.builtins.diagnostics
+local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
   debug = false,
@@ -17,6 +17,7 @@ null_ls.setup({
     -- formatting.rubocop,
     -- formatting.prettier.with { extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } },
     -- formatting.black.with { extra_args = { "--fast" } },
+    diagnostics.vale,
     -- diagnostics.flake8,
   },
 })
